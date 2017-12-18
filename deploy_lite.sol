2e71f64a601777166366bae245c63719b4e16a41
VR <rudakov044@gmail.com>
2017-12-18 18:11:17 +0200

    fc

---
+++ deploy_lite.sol
@@ -0,0 +1,77 @@
+pragma solidity ^0.4.16;
+
+interface token {
+    function transfer(address _to, uint256 _value) public returns (bool success);
+}
+
+contract Crowdsale {
+    address public beneficiary;
+    uint public amountRaised;
+    uint public deadline;
+    uint public price;
+    token public tokenReward;
+    mapping(address => uint256) public balanceOf;
+    bool crowdsaleClosed = false;
+
+    event GoalReached(address recipient, uint totalAmountRaised);
+    event FundTransfer(address backer, uint amount, bool isContribution);
+
+    /**
+     * Constrctor function
+     *
+     * Setup the owner
+     */
+    function Crowdsale(
+        address addressOfTokenUsedAsReward
+    ) internal {
+        beneficiary = msg.sender;
+        price = 1 ether;
+        tokenReward = token(addressOfTokenUsedAsReward);
+    }
+
+    /**
+     * Fallback function
+     *
+     * The function without name is the default function that is called whenever anyone sends funds to a contract
+     */
+   function () public payable {
+        require(!crowdsaleClosed);
+        uint amount = msg.value;
+        balanceOf[msg.sender] += amount;
+        amountRaised += amount;
+        tokenReward.transfer(msg.sender, amount / price);
+        FundTransfer(msg.sender, amount, true);
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == beneficiary);
+        _;
+    }
+
+    function goalManagment(bool statement) public onlyOwner {
+        if (statement == true) {
+            GoalReached(beneficiary, amountRaised);
+            crowdsaleClosed = true;
+            safeWithdrawal();
+        }
+
+    }
+
+    /**
+     * Withdraw the funds
+     *
+     * Checks to see if goal or time limit has been reached, and if so, and the funding goal was reached,
+     * sends the entire amount to the beneficiary. If goal was not reached, each contributor can withdraw
+     * the amount they contributed.
+     */
+    function safeWithdrawal() internal {
+        if (crowdsaleClosed == true && beneficiary == msg.sender) {
+            if (beneficiary.send(amountRaised)) {
+                FundTransfer(beneficiary, amountRaised, false);
+            } else {
+                //If we fail to send the funds to beneficiary, unlock funders balance
+                crowdsaleClosed = false;
+            }
+        }
+    }
+}
