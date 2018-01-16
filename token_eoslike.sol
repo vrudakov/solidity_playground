2de154fd463b67e313b2a290a768fe95a2db4e0d
liderako <trotillostudio@gmail.com>
2018-01-16 18:57:26 +0200

    Update token_eoslike.sol
    deleted function burnFrom and change style

--- token_eoslike.sol
+++ token_eoslike.sol
@@ -3,18 +3,15 @@ pragma solidity ^0.4.16;
 contract safeMath {
 
 	function add( uint256 x, uint256 y ) internal pure returns ( uint256 z ) {
-		
 		assert( ( z = x + y ) >= x );
 	}
 
 	function sub( uint256 x, uint256 y ) internal pure returns ( uint256 z ) {
-		
 		assert( ( z = x - y ) <= x );
 	}
 }
 
 contract ERC20 {
-
 	function 	totalSupply() public constant returns (uint);
 	function    balanceOf(address who) public view returns (uint256);
 	function    allowance(address owner, address spender) public view returns (uint256);
@@ -28,7 +25,6 @@ contract ERC20 {
 }
 
 contract 	baseToken is ERC20, safeMath {
-
 	uint256			_totalSupply;
 	address			_admin;
 	string 			_name;
@@ -41,9 +37,7 @@ contract 	baseToken is ERC20, safeMath {
 	event Burn( address indexed from, uint256 value );
 
 	function 	baseToken( string nameToken, string symbolToken, uint256 supply, uint8 decimals ) public {
-		
 		uint256 	balance;
-
 
 		balance = supply * 10 ** uint256( decimals );
 		_name = nameToken;
@@ -55,7 +49,6 @@ contract 	baseToken is ERC20, safeMath {
 	}
 
 	modifier 	assertAdmin() {
-		
 		if ( msg.sender != _admin ) {
 			assert( false );
 		}
@@ -63,81 +56,51 @@ contract 	baseToken is ERC20, safeMath {
 	}	
 
 	function 	totalSupply() public constant returns ( uint256 ) {
-
 		return _totalSupply;
 	}
 
 	function 	balanceOf( address user ) public view returns ( uint256 ) {
-		
 		return _balanceOf[user];
 	}
 
 	function 	allowance( address owner, address spender ) public view returns ( uint256 ) {
-		
 		return _allowance[owner][spender];
 	}
 
 	function 	transfer( address to, uint amount ) public returns ( bool ) {
-
 		assert(_balanceOf[msg.sender] >= amount);
-
 		_balanceOf[msg.sender] = sub( _balanceOf[msg.sender], amount );
 		_balanceOf[to] = add( _balanceOf[to], amount );
-
 		Transfer( msg.sender, to, amount );
-
 		return true;
 	}
 
 	function 	transferFrom( address from, address to, uint amount ) public returns ( bool ) {
-
 		assert( _balanceOf[from] >= amount );
 		assert( _allowance[from][msg.sender] >= amount );
-
 		_allowance[from][msg.sender] = sub( _allowance[from][msg.sender], amount );
 		_balanceOf[from] = sub( _balanceOf[from], amount );
 		_balanceOf[to] = add( _balanceOf[to], amount );
-
 		Transfer( from, to, amount );
-
 		return true;
 	}
 
 	function 	approve( address spender, uint256 amount ) public returns ( bool ) {
-
 		_allowance[msg.sender][spender] = amount;
-
 		Approval( msg.sender, spender, amount );
-
 		return true;
 	}
 
 	function 	addTotalSupply( uint256 newBalance ) public assertAdmin {
-		
 		_balanceOf[_admin] = add( _balanceOf[_admin], newBalance );
 		_totalSupply = add( _totalSupply, newBalance );
 	}
 
 	function 	burn( uint256 value ) public returns ( bool success ) {
-		
 		assert( _balanceOf[msg.sender] >= value );	// Check if the sender has enough
-		
 		_balanceOf[msg.sender] -= value;			// Subtract from the sender
 		_totalSupply -= value;						// Updates _totalSupply
 		Burn( msg.sender, value );
-		return true;
-	}
-
-	function 	burnFrom( address from, uint256 value ) public returns ( bool success ) {
-
-		assert( _balanceOf[from] >= value );					// Check if the targeted balance is enough
-		assert( value <= _allowance[from][msg.sender] );		// Check allowance
-
-		_balanceOf[from] -= value;							// Subtract from the targeted balance
-		_allowance[from][msg.sender] -= value;				// Subtract from the sender's allowance
-		_totalSupply -= value;								// Update _totalSupply
-		
-		Burn( from, value );
 		return true;
 	}
 }
